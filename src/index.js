34dc06b47969fbb392507ca3f087e101aedb09f6
EmanuelStoef <150154248+EmanuelStoef@users.noreply.github.com>
2025-11-24 08:00:21 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
- 
-// Define your routes
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Express on Vercel!' });
-});
- 
+
+app.set('view engine', 'ejs');
+app.set('views', './views')
+
+require("../db/db")(app);
+require("../rotas/home")(app);
+require("../rotas/getnoticias")(app);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
