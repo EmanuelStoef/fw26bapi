b9a98d137530b86f742e5f422ed647bfbdc15c7c
EmanuelStoef <150154248+EmanuelStoef@users.noreply.github.com>
2025-11-28 09:13:13 -0300

    atualizado urlencode e params

--- rotas/getidnoticias.js
+++ rotas/getidnoticias.js
@@ -2,7 +2,7 @@ const { ObjectId } = require("mongodb");
 
 module.exports = (app) => {
   app.get('/noticias/id/:id', async (req, res) => {
-    const id = ObjectId.createFromHexString(req.body.id)
+    const id = ObjectId.createFromHexString(req.params.id)
     await app.DBClient.connect();
     const noticias = await app.DBClient.db('portalnoticias')
       .collection('noticias').find({_id:id}).toArray();
